f3a62b25fa7a928bbca47d7315e482032ccdc637
David Austin <david.austin.m@gmail.com>
2024-11-25 08:51:16 -0500

    improved docker container

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-echo '/usr/lib/python3/dist-packages' > /usr/local/lib/python3.12/site-packages/louis.pth
-prefig init
 prefig new
 prefig examples
 cat .my_bashrc >> ~/.bashrc
